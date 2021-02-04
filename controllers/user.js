1ae220392450a17c06666a17e4747ea4d0f33d93
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-05 00:18:40 +0600

    Api validation

--- controllers/user.js
+++ controllers/user.js
@@ -11,6 +11,26 @@ const userController = {
     createUser: (req, res, next) => {
         const { id, firstName, lastName } = req.body
 
+        let errors = {}
+        let errorMessage = ''
+        let isBodyValid = true
+
+        if (!firstName || typeof firstName !== 'string' || firstName.trim().length === 0) {
+            isBodyValid = false
+            errors[firstName] = 'This should be a string & not empty'
+        }
+
+        if(!isBodyValid) {
+            return res.status(400).json({
+                success: isBodyValid,
+                errorMessage: 'Kindly correct the errors',
+                errors,
+            })
+        }
+
+
+
+        //only execute below data when I am sure all is correct
         const userPayLoad = {
             id,
             firstName,
