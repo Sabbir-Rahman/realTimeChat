6e324b5ac074b58abc26591d3e03ce10a4b2f5f7
Md Sabbir Rahman <56318666+Sabbir-Rahman@users.noreply.github.com>
2021-02-05 11:40:42 +0600

    create user api complete

--- models/User.js
+++ models/User.js
@@ -1,88 +1,40 @@
 import mongoose from "mongoose";
 import { v4 as uuidv4 } from "uuid";
 
-export const CHAT_ROOM_TYPES = {
-  STUDENT_TO_STUDENT: "student-to-student",
-  STUDENT_TO_TEACHER: "student-to-teacher",
-  STUDENT_TO_ADMIN: "student-to-admin",
+export const USER_TYPES = {
+  STUDENT: "student",
+  TEACHER: "teacher",
+  ADMIN: "admin",
 };
 
-const chatRoomSchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     _id: {
       type: String,
       default: () => uuidv4().replace(/\-/g, ""),
     },
-    userIds: Array,
+    firstName: String,
+    lastName: String,
     type: String,
-    chatInitiator: String,
   },
   {
     timestamps: true,
-    collection: "chatrooms",
+    collection: "users",
   }
 );
 
-/**
- * @param {String} userId - id of user
- * @return {Array} array of all chatroom that the user belongs to
- */
-chatRoomSchema.statics.getChatRoomsByUserId = async function (userId) {
+//static method
+userSchema.statics.createUser = async function (
+	    firstName, 
+    	lastName, 
+    	type
+) {
   try {
-    const rooms = await this.find({ userIds: { $all: [userId] } });
-    return rooms;
+    const user = await this.create({ firstName, lastName, type });
+    return user;
   } catch (error) {
     throw error;
   }
 }
 
-/**
- * @param {String} roomId - id of chatroom
- * @return {Object} chatroom
- */
-chatRoomSchema.statics.getChatRoomByRoomId = async function (roomId) {
-  try {
-    const room = await this.findOne({ _id: roomId });
-    return room;
-  } catch (error) {
-    throw error;
-  }
-}
-
-/**
- * @param {Array} userIds - array of strings of userIds
- * @param {String} chatInitiator - user who initiated the chat
- * @param {CHAT_ROOM_TYPES} type
- */
-chatRoomSchema.statics.initiateChat = async function (userIds, type, chatInitiator) {
-  try {
-    const availableRoom = await this.findOne({
-      userIds: {
-        $size: userIds.length,
-        $all: [...userIds],
-      },
-      type,
-    });
-    if (availableRoom) {
-      return {
-        isNew: false,
-        message: 'retrieving an old chat room',
-        chatRoomId: availableRoom._doc._id,
-        type: availableRoom._doc.type,
-      };
-    }
-
-    const newRoom = await this.create({ userIds, type, chatInitiator });
-    return {
-      isNew: true,
-      message: 'creating a new chatroom',
-      chatRoomId: newRoom._doc._id,
-      type: newRoom._doc.type,
-    };
-  } catch (error) {
-    console.log('error on start chat method', error);
-    throw error;
-  }
-}
-
-export default mongoose.model("ChatRoom", chatRoomSchema);
+export default mongoose.model("User", userSchema);
